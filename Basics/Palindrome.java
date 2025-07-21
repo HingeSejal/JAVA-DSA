3c5bcaba04927695247eeeeb99ce17384095f695
HingeSejal <sejalhinge4@gmail.com>
2025-07-21 23:25:20 +0530

    Adv parttern printing

---
+++ Basics/Palindrome.java
@@ -0,0 +1,27 @@
+package Basics;
+
+public class Palindrome {
+    public static void main(String args[]){
+        /*
+                    11
+                   2112
+                  321123
+                 43211234
+                5432112345
+        
+        */
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i ;j++){
+               System.out.print(" ");
+             }
+             for(int j=i;j>=1 ;j--){ // i times
+               System.out.print(j);
+             }
+             for(int j=1;j<=i ;j++){ // i times
+               System.out.print(j);
+             }
+            System.out.println();
+        }
+
+    }
+}
--- Basics/SolidRhom.java
+++ Basics/SolidRhom.java
@@ -5,10 +5,11 @@ public class SolidRhom {
          int n=5;
          /*
                *****
-            *****
+             *****
+           *****
          *****
-     *****
-*****
+       *****
+       
          */
          for(int i=1;i<=n;i++){
             for(int j=1;j<=n-i;j++){
