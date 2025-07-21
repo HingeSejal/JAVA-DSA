505a2868a703dbb001422efe3d42c0aee1810fb5
HingeSejal <sejalhinge4@gmail.com>
2025-07-21 08:33:10 +0530

    done with patterns

--- Basics/Nesting.java
+++ Basics/Nesting.java
@@ -169,6 +169,28 @@ public class Nesting {
             }
            System.out.println();
         }
+
+        /*
+
+                1
+                0 1
+                1 0 1
+                0 1 0 1
+                1 0 1 0 1
+
+        */
+        
+        for(int i=1;i<=5;i++){ //rows
+            for(int j=1;j<=i;j++){ //col=no of rows
+              if((i+j)%2==0){
+                System.out.print("1");
+              }
+              else{
+                System.out.print("0");
+              }
+            }
+           System.out.println();
+        }
     }
 }
 
