66ffa28eab2c2e01bdafa7f24ea12ee508e97b9a
HingeSejal <sejalhinge4@gmail.com>
2025-07-22 08:30:51 +0530

    dtype

--- Basics/Dtype.java
+++ Basics/Dtype.java
@@ -1,7 +1,7 @@
 package Basics;
 import java.util.*;
 public class Dtype {
-    public static void main(String[] args){
+    public static void main(String args[]){
         // String name ="SEJA PVT" ;
         int age = 20;
         double price= 67.7623;
