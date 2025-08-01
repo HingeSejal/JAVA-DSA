47ce410ddb9956a0be365c9bc4ec8961d6fff57d
HingeSejal <sejalhinge4@gmail.com>
2025-08-02 01:14:27 +0530

    basics polishing

--- Basics/Dtype.java
+++ Basics/Dtype.java
@@ -5,35 +5,55 @@ public class Dtype {
         // String name ="SEJA PVT" ;
         int age = 20;
         double price= 67.7623;
-        int a=35;
+        float a = 3434.54F;
+        long l=565L;
+        int b=35;
 
         // name = "HINGE"; //can be modified
 
 
         Scanner sc= new Scanner(System.in);
-        String name = sc.next();//takes only first word as a token
+        String name = sc.next();//takes only first word as a token - print until the void space value
+        sc.nextLine();
         String name2 = sc.nextLine(); //takes entire line
         System.out.println(name2);
+        System.out.println(name2.charAt(2));
+        System.out.println(name2.charAt(4));
+
+        char c = sc.next().charAt(1);
+        System.out.println(c);
+
+        // String s2 = """
+        //         hello dear bachho
+        //         """;
+                
     }
 }
 
 /*
 Datatypes in Java
 1) Primitive
-byte => 8bits
-short
-char(2)
-boolean(1)
-int(4)
-long(8)
-float(4)
-double(8)
+    a) numeric
+        int(4)
+            long(8)
+            short  
+            byte => 8bits
+        float
+            float(4)
+            double(8)
+        
+
+    b) non-numeric
+        char(2)
+        boolean(1)
+
 
 2)NonPrimitive
 String
 Array
-Class
-Obj
-Interface
+User defined
+    Class
+    Obj
+    Interface
 
 */
---
+++ Basics/typeCasting.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class typeCasting {
+     public static void main(String args[]){
+        long x=10000;
+        int y =(int) x; //long to int - violets the range
+     }
+}
+
+/*
+Widening type casting:
+    Auto by java
+Explicit type casting:
+    user defined
+
+//increasing Order of size
+double -> float -> long -> int -> short -> byte
+*/
