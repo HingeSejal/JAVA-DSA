535956928b6652ca17d6879eaf6c002087787115
HingeSejal <sejalhinge4@gmail.com>
2025-08-03 00:48:21 +0530

    largest

--- Basics/largest.java
+++ Basics/largest.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class largest {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Range:");
         int n= sc.nextInt();
-
+        System.out.println("Enter nos:");     
         int maxValue = Integer.MIN_VALUE;
         for(int i=1;i<=n;i++){
             int current = sc.nextInt();
             maxValue = Math.max(maxValue,current);
         }
-        System.out.println(maxValue);
+        System.out.println("Largest Value:"+maxValue);
     }
 }
