a6574468f5a8816589f804795ef4e0b81351390d
HingeSejal <sejalhinge4@gmail.com>
2025-08-03 00:34:25 +0530

    largest of N num

---
+++ Basics/largest.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class largest {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+
+        int maxValue = Integer.MIN_VALUE;
+        for(int i=1;i<=n;i++){
+            int current = sc.nextInt();
+            maxValue = Math.max(maxValue,current);
+        }
+        System.out.println(maxValue);
+    }
+}
