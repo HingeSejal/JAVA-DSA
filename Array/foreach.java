aa951c5138d36c6b5be997eae9d856d48b0bddb0
HingeSejal <sejalhinge4@gmail.com>
2025-08-03 09:44:12 +0530

    loop

---
+++ Array/foreach.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class foreach {
+    public static void main(String args[]){
+        int[] arr={5,3,2,11,23};
+        for(int num:arr){
+            System.out.println(num);
+        }
+    }
+}
