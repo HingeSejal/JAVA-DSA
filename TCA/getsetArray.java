e6065a8a32f34bbcdf406776e9d4a95325f45646
HingeSejal <sejalhinge4@gmail.com>
2025-09-29 19:53:08 +0530

    Starting with array

--- TCA/getsetArray.java
+++ TCA/getsetArray.java
@@ -1,90 +1,81 @@
 package TCA;
-import java.io.* ;
 
-class Student {
-	
-	private int rno;
-	private String name;
-	private double per;
-	
-	public int getRno() {
-		return rno;
-	}
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+class Student_pgm {
+    
+    private int rno;
+    private String name;
+    private double per;
+    
+    public int getRno() {
+        return rno;
+    }
 
-	public void setRno(int rno) {
-		this.rno = rno;
-	}
+    public void setRno(int rno) {
+        this.rno = rno;
+    }
 
+    public String getName() {
+        return name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public double getPer() {
+        return per;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setPer(double per) {
+        this.per = per;
+    }
 
-
-	public double getPer() {
-		return per;
-	}
-
-
-	public void setPer(double per) {
-		this.per = per;
-	}
-
-
-	   void display(){
-	    System.out.println("------------display content----------");
-	    System.out.println("rno:"+rno);
-	   System.out.println("name:"+name);
-	   System.out.println("percent:"+per);
-	   }
-	
+    void display() {
+        System.out.println("------------display content----------");
+        System.out.println("Roll No: " + rno);
+        System.out.println("Name: " + name);
+        System.out.println("Percent: " + per);
+    }
 }
 
-public class getsetArray{
-	
-  public static void main(String[] args) throws Exception{ 
-	  
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	  
-	  System.out.println("How many student:");
-	  int n= Integer.parseInt(br.readLine());
-	  
-	  Student S[] = new Student[n];
-	  
-	  for(int i=0;i<S.length;i++) {
-		  System.out.println("Enter the Roll No:");
-		  int r = Integer.parseInt(br.readLine());
-		  
-		  System.out.println("Enter the Name:");
-		  String nm = br.readLine();
-		  
-		  System.out.println("Enter percent:");
-		  double p = Double.parseDouble(br.readLine());
-		  
-		  S[i] = new Student();
-		  
-	  }
-		  for(int i=0;i<S.length;i++) {
-		  S[i].getRno();
-		  S[i].getName();
-		  S[i].getPer();
-		  
-	  }
-	  
-	  for(int i=0;i<S.length;i++) {
-		  
-		  S[i].setRno(76);
-		  S[i].setName("sej");
-		  S[i].setPer(89);
-		  
-	  }
-	  
-	  
-  }
+public class getsetArray {
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("How many students:");
+        int n = Integer.parseInt(br.readLine());
+        
+        Student_pgm[] S = new Student_pgm[n];
+        
+        // Input loop
+        for (int i = 0; i < S.length; i++) {
+            System.out.println("\nEnter details for student " + (i + 1));
+            
+            System.out.print("Enter the Roll No: ");
+            int r = Integer.parseInt(br.readLine());
+            
+            System.out.print("Enter the Name: ");
+            String nm = br.readLine();
+            
+            System.out.print("Enter Percent: ");
+            double p = Double.parseDouble(br.readLine());
+            
+            // create object & set values
+            S[i] = new Student_pgm();
+            S[i].setRno(r);
+            S[i].setName(nm);
+            S[i].setPer(p);
+        }
+        
+        // Display loop
+        System.out.println("\n===== Student Details =====");
+        for (int i = 0; i < S.length; i++) {
+            S[i].display();
+        }
+    }
 }
