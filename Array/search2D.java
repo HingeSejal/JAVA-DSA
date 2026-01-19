85984ec918a79c732dbc4df49d3a64213ef23add
HingeSejal <sejalhinge4@gmail.com>
2026-01-20 00:57:26 +0530

    2d ARRAY codes

---
+++ Array/search2D.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.Scanner;
+
+public class search2D {
+    public static void main(String args[]){
+        // Implement 2D array searching element logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows and columns:");
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] numbers = new int[rows][cols];
+
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter element to search:"); 
+        int x=sc.nextInt();
+
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                if(numbers[i][j]==x){
+                    System.out.println(x+ " is found at position : " + i+ "," +j);
+                    return;
+                }
+             }
+         }
+    }
+}
---
+++ Array/spiral2D.java
@@ -0,0 +1,66 @@
+package Array;
+
+import java.util.Scanner;
+
+public class spiral2D {
+    public static void main(String args[]){
+        // Implement spiral traversal logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows and columns:");
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();   
+        
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter matrix elements:");
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Elements:");
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Spiral traversal:");
+        
+        int rowStart = 0;
+        int rowEnd = rows - 1;
+        int colStart = 0;
+        int colEnd = cols - 1;
+
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            // Traverse right
+            for (int i = colStart; i <= colEnd; i++) {
+                System.out.print(matrix[rowStart][i] + " ");
+            }
+            rowStart++;
+
+            // Traverse down
+            for (int i = rowStart; i <= rowEnd; i++) {
+                System.out.print(matrix[i][colEnd] + " ");
+            }
+            colEnd--;
+
+            // Traverse left
+                for (int i = colEnd; i >= colStart; i--) {
+                    System.out.print(matrix[rowEnd][i] + " ");
+                }
+                rowEnd--;
+            
+
+            // Traverse up
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    System.out.print(matrix[i][colStart] + " ");
+                }
+                colStart++;
+            
+            System.out.println();
+        }
+        }
+    }
