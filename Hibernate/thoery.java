50a3d9384bbca02b309270df7e8fc240bc16cd62
HingeSejal <sejalhinge4@gmail.com>
2026-03-27 16:10:00 +0530

    hibernate intro

---
+++ Hibernate/thoery.java
@@ -0,0 +1,15 @@
+/* 
+
+Hibernate is Object-Relational Mapping (ORM) framework for Java. 
+
+ues ?
+Hibernate provides a powerful and flexible API for performing database operations, such as querying, inserting, updating, and deleting data.
+It also supports features like lazy loading, caching, and transaction management, 
+which can improve performance and scalability of applications.
+
+what is ORM ?
+ORM stands for Object-Relational Mapping.
+It is a programming technique that allows developers to interact with databases using object-oriented programming principles.
+ORM frameworks like Hibernate provide a way to map Java objects to database tables.
+
+*/
