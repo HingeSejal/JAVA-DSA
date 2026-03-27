2f15ec421421bcf4dc6494c9a9b3ac5297821871
HingeSejal <sejalhinge4@gmail.com>
2026-03-27 16:09:46 +0530

    collections in java

---
+++ AdvJava/Collection/theory.java
@@ -0,0 +1,16 @@
+/*
+
+Java Array is  created dynamically and it static(fixed) in size. 
+Once the size of the array is defined, it cannot be changed. 
+If we want to change the size of the array, create a new array and copy the elements from the old to new array. 
+This can be inefficient and time-consuming, especially if the array is large.
+
+In contrast, Java Collections framework are dynamic in size.
+They can grow and shrink as needed, which makes them more flexible and efficient for managing data.
+
+Container which holds the data in the form of objects is called collection.
+
+The Java Collections framework provides a set of interfaces and classes 
+that implement various data structures, such as lists, sets, queues, and maps.
+
+*/
