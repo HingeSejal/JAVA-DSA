2df94b40589dc556696ac446d794fcf7ac7a8c35
HingeSejal <sejalhinge4@gmail.com>
2026-03-28 08:42:25 +0530

    collections in java

--- AdvJava/Collection/theory.java
+++ AdvJava/Collection/theory.java
@@ -13,4 +13,22 @@ Container which holds the data in the form of objects is called collection.
 The Java Collections framework provides a set of interfaces and classes 
 that implement various data structures, such as lists, sets, queues, and maps.
 
+e.g imagine if u dont know the no of records in database on system and on application side u want to save data in memory
+    also even if u get no of records if new record need to be added
+    then in this scenario array cant be used because of its fixed size 
+
+
+                                Collection framework
+                                 /                 \
+  (data is in form of values) Collection            Map (Data is in form of key value pair)
+                            /           \                                   |
+                        List            Set                             HashMap
+(Duplicates allowed ,insertion order)  (unique and sorted values)       LinkedHashMap
+                        |                 |                             Hashtable
+                    ArrayList           HashSet
+                    LinkedList          TreeSet 
+                    Stack
+                    Vector
+                    PriorityQueue
+
 */
