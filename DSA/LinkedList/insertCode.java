a26e88a43a8a201b000d45f6f2274cfa9c61a760
HingeSejal <sejalhinge4@gmail.com>
2026-06-27 01:39:56 +0530

    Diving intoLinkedList

---
+++ DSA/LinkedList/insertCode.java
@@ -0,0 +1,67 @@
+package DSA.LinkedList;
+
+public class insertCode {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public class Node{
+        private int data;
+        private Node next;
+
+        public Node(int data){
+            this.data = data;
+        }
+
+        public Node(int data , Node next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public insertCode(){
+        this.size = 0;
+    }
+
+    public void insertFirstNode(int data){
+        Node node = new Node(data);
+        node.next=head;
+        head = node;
+        if(tail==null){
+            tail = node;
+        }
+        size++;
+    }
+
+    public void insertLLastNode(int data){
+        Node node = new Node(data);
+        if(tail==null){
+            insertFirstNode(data);
+            return;
+        }
+        tail.next=node;
+        tail = node;
+        
+        size++;
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+    public static void main(String[] args) {
+        insertCode list = new insertCode();
+        list.insertFirstNode(23);
+        list.insertFirstNode(45);
+        list.insertFirstNode(34);
+        list.insertFirstNode(89);
+        list.insertFirstNode(98);
+        list.display();
+    }
+    
+}
